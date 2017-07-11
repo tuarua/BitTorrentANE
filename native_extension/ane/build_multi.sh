8d57f9d1f85ecd67eb6dfc9e3946c214d85900e0
Eoin Landy <el111@yahoo.com>
2017-07-11 20:58:39 +0100

    1.3.4 final compile

--- native_extension/ane/build_multi.sh
+++ native_extension/ane/build_multi.sh
@@ -8,7 +8,7 @@ echo $pathtome
 
 PROJECT_NAME=BitTorrentANE
 
-AIR_SDK="/Applications/Adobe Flash Builder 4.7/sdks/4.6.0"
+AIR_SDK="/Users/User/sdks/AIR/AIRSDK_26"
 echo $AIR_SDK
 
 #Setup the directory.
@@ -18,20 +18,14 @@ if [ ! -d "$pathtome/platforms" ]; then
 mkdir "$pathtome/platforms"
 fi
 
+if [ -d "$pathtome/platforms/mac" ]; then
+rm -r "$pathtome/platforms/mac"
+fi
 
 if [ ! -d "$pathtome/platforms/mac" ]; then
 mkdir "$pathtome/platforms/mac"
 mkdir "$pathtome/platforms/mac/release"
-mkdir "$pathtome/platforms/mac/debug"
 fi
-
-if [ ! -d "$pathtome/platforms/win" ]; then
-mkdir "$pathtome/platforms/win"
-mkdir "$pathtome/platforms/win/release"
-mkdir "$pathtome/platforms/win/debug"
-fi
-
-
 
 
 #Copy SWC into place.
@@ -45,19 +39,13 @@ unzip "$pathtome/$PROJECT_NAME.swc" "library.swf" -d "$pathtome"
 #Copy library.swf to folders.
 echo "Copying library.swf into place."
 cp "$pathtome/library.swf" "$pathtome/platforms/mac/release"
-cp "$pathtome/library.swf" "$pathtome/platforms/mac/debug"
-cp "$pathtome/library.swf" "$pathtome/platforms/win/release"
-cp "$pathtome/library.swf" "$pathtome/platforms/win/debug"
+cp "$pathtome/library.swf" "$pathtome/platforms/win/x86/release"
+cp "$pathtome/library.swf" "$pathtome/platforms/win/x64/release"
 
 #Copy native libraries into place.
 echo "Copying native libraries into place."
 cp -R -L "$pathtome/../../native_library/mac/$PROJECT_NAME/Build/Products/Release/$PROJECT_NAME.framework" "$pathtome/platforms/mac/release"
-cp -R -L "$pathtome/../../native_library/mac/$PROJECT_NAME/Build/Products/Debug/$PROJECT_NAME.framework" "$pathtome/platforms/mac/debug"
 
-if [ -d "$pathtome/../../native_library/win/$PROJECT_NAME/Release" ]; then
-cp -R -L "$pathtome/../../native_library/win/$PROJECT_NAME/Release/$PROJECT_NAME.dll" "$pathtome/platforms/win/release"
-cp -R -L "$pathtome/../../native_library/win/$PROJECT_NAME/Release/$PROJECT_NAME.dll" "$pathtome/platforms/win/debug"
-fi
 
 #Run the build command.
 echo "Building Release."
@@ -65,25 +53,9 @@ echo "Building Release."
 -target ane "$pathtome/$PROJECT_NAME.ane" "$pathtome/extension_multi.xml" \
 -swc "$pathtome/$PROJECT_NAME.swc" \
 -platform MacOS-x86-64 -C "$pathtome/platforms/mac/release" "$PROJECT_NAME.framework" "library.swf" \
--platform Windows-x86 -C "$pathtome/platforms/win/release" "$PROJECT_NAME.dll" "library.swf"
+-platform Windows-x86 -C "$pathtome/platforms/win/x86/release" "$PROJECT_NAME.dll" "library.swf" \
+-platform Windows-x86-64 -C "$pathtome/platforms/win/x64/release" "$PROJECT_NAME.dll" "library.swf"
 
-echo "Building Debug."
-"$AIR_SDK"/bin/adt -package \
--target ane "$pathtome/$PROJECT_NAME-debug.ane" "$pathtome/extension_multi.xml" \
--swc "$pathtome/$PROJECT_NAME.swc" \
--platform MacOS-x86-64 -C "$pathtome/platforms/mac/debug" "$PROJECT_NAME.framework" "library.swf" \
--platform Windows-x86 -C "$pathtome/platforms/win/debug" "$PROJECT_NAME.dll" "library.swf"
 
-if [[ -d "$pathtome/debug" ]]
-then
-rm -r "$pathtome/debug"
-fi
-z
-
-mkdir "$pathtome/debug"
-unzip "$pathtome/$PROJECT_NAME-debug.ane" -d  "$pathtome/debug/$PROJECT_NAME.ane/"
-
-#rm -r "$pathtome/platforms"
 rm "$pathtome/$PROJECT_NAME.swc"
 rm "$pathtome/library.swf"
-rm "$pathtome/$PROJECT_NAME-debug.ane"
